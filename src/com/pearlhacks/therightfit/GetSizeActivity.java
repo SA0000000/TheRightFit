4aa1b033997cb2cc877dd16db3fd95b7eea420fd
SJA0000016 <sarahj.andrabi@gmail.com>
2014-03-23 11:59:04 -0400

    Updated_text_views_adding_text

--- src/com/pearlhacks/therightfit/GetSizeActivity.java
+++ src/com/pearlhacks/therightfit/GetSizeActivity.java
@@ -3,17 +3,38 @@ package com.pearlhacks.therightfit;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class GetSizeActivity extends Activity {
 
+	private String txt_tops_size;
+	private String txt_dress_size;
+	private String txt_bottom_size;
+	private String txt_user_name;
+	private String txt_brand_name;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Bundle extras = getIntent().getExtras();
+		Bundle extras = getIntent().getExtras();		
 		int bid = extras.getInt("BRAND_ID",1);
 		int uid = extras.getInt("USER_ID",1);
-		
 		setContentView(R.layout.activity_get_size);
+		
+		//Use these when you have the string array with all the values to set
+		TextView top =(TextView)findViewById(R.id.txt_top_size);
+		TextView dress =(TextView)findViewById(R.id.txt_dress_size);
+		TextView bottom =(TextView)findViewById(R.id.txt_bottom_size);
+		TextView user_name =(TextView)findViewById(R.id.txt_usr_name);
+		TextView brand_name =(TextView)findViewById(R.id.txt_brand_name);
+		top.setText(txt_tops_size);
+		dress.setText(txt_dress_size);
+		bottom.setText(txt_bottom_size);
+		user_name.setText(txt_user_name);
+		brand_name.setText(txt_brand_name);
+		
+		
+		
+		
 	}
 
 	@Override
